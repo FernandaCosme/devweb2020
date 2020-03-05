3a9f728161937178f4d24698e0a9574f488c31de
816120732 <816120732@VLELI0104.animaedu.intranet>
2020-03-04 22:29:45 -0300

    teste 01

---
+++ Teste01/src/br/com/fh/app01/Helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.fh.app01;
+
+/**
+ * @author 816120732
+ *
+ */
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi World");
+	}
+
+}
